394609750d13e9461899c5a529baa229eae9b87b
Jeff Ripke <jripke74@gmail.com>
2023-10-04 09:24:29 -0700

    add express-session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("cookie").split("=")[1].trim();
+  // const isLoggedIn = req.get("cookie").split("=")[1].trim();
+  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
